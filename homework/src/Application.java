3505e65d26176a40ac1d818040cb227f853a6fb6
Rohit <rohitum@gmail.com>
2017-08-08 19:21:39 -0700

    moved notes and homework

---
+++ homework/src/Application.java
@@ -0,0 +1,14 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int grade;
+		
+		if (grade ==  5)
+			System.out.println("Elementary School")
+	Else if (grade >=8 && grade <=8);
+	}
+
+}
